1a8dca9c64e628e0dfb5e64bd9ee9079026db45d
Pietru0 <74650785+Pietru0@users.noreply.github.com>
2022-03-16 15:38:52 +0100

    SceneManagement done
    All buttons go to their respective scene
    
    Next time will attempt to create instructions navigation page

---
+++ Dissertation Project/Assets/Scripts/SceneController.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class SceneController : MonoBehaviour
+{
+    public void ModeSelectBTN()
+    {
+        SceneManager.LoadScene("Mode Select");
+    }
+
+        public void MenuBTN()
+    {
+        SceneManager.LoadScene("Menu");
+    }
+    public void InstructionsBTN()
+    {
+        SceneManager.LoadScene("Instructions");
+    }
+
+    public void SongSelectBTN()
+    {
+        SceneManager.LoadScene("Song Selection");
+    }
+
+    public void LearningBTN()
+    {
+        SceneManager.LoadScene("Learning");
+        //not sure if I want to include this yet, but it's here just incase I do
+    }
+    public void QuitBTN()
+    {
+        Application.Quit();
+        Debug.Log("Quit Game");
+    }
+}
