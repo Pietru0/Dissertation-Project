aa0f402bc7458351ab9ca5f79c25f514b75cfa93
Pietru0 <74650785+Pietru0@users.noreply.github.com>
2022-03-25 11:25:24 +0100

    Attempting to Optimize note creation
    - not completely there yet, continuing with regular method for the time being

--- Dissertation Project/Assets/Scripts/Game Mechanics/NoteScroller.cs
+++ Dissertation Project/Assets/Scripts/Game Mechanics/NoteScroller.cs
@@ -5,14 +5,25 @@ using UnityEngine;
 public class NoteScroller : MonoBehaviour
 {
     public float BPM;
-
+    private float travelSpeed;
     public bool songStarted;
+
+    [SerializeField] private float songTimer = 0;
+
+    [SerializeField] private float startPos = 15;
+    [SerializeField] private Transform markerPos;
+
+    private float totalTime;
+    NoteData.NoteInfo nextNote;
 
     // Start is called before the first frame update
     void Start()
     {
-        BPM = BPM / 60;
+        travelSpeed = BPM / 60;
+        totalTime = (startPos - markerPos.position.x) / travelSpeed;
+        Debug.Log(totalTime);
         //setting how fast notes should move per second
+        
     }
 
     // Update is called once per frame
@@ -28,8 +39,11 @@ public class NoteScroller : MonoBehaviour
 
         else
         {
-            transform.position -= new Vector3(BPM * Time.deltaTime, 0f);
+            songTimer+=Time.deltaTime;
+            transform.position -= new Vector3(travelSpeed * Time.deltaTime, 0f);
             //scroll notes at y-axis according to set BPM
         }
+
+
     }
 }
---
+++ Dissertation Project/Assets/Scripts/NoteData.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "ScriptableObject/NoteData")]
+public class NoteData : ScriptableObject
+{
+
+    public enum NoteColor
+    {
+        Red, Green, Blue
+    }
+
+    //class that cannot be null
+    [System.Serializable]
+    public struct NoteInfo
+    {
+        public NoteColor color;
+        public float timeStamp;
+    }
+
+    public NoteInfo[] notes;
+
+    public NoteInfo GetNote(int index)
+    {
+        return notes[index];
+    }
+}
